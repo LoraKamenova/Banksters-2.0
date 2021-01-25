e9347995f7bd519d02e48a7b9c63e4383c411594
Lora <cruzeiro@abv.bg>
2021-01-25 19:31:26 +0200

    New user navigation added

--- src/main/java/softuni/banksters/web/interceptors/GreetingInterceptor.java
+++ src/main/java/softuni/banksters/web/interceptors/GreetingInterceptor.java
@@ -21,13 +21,13 @@ public class GreetingInterceptor extends HandlerInterceptorAdapter {
             LocalTime time = LocalTime.now();
             int hrs = time.getHour();
             if (hrs >= 0 && hrs < 6) {
-                model.addObject("greeting", "\uD835\uDD3E\uD835\uDD60\uD835\uDD60\uD835\uDD55 \uD835\uDD5F\uD835\uDD5A\uD835\uDD58\uD835\uDD59\uD835\uDD65, \uD835\uDD40\uD835\uDD5F\uD835\uDD67\uD835\uDD56\uD835\uDD64\uD835\uDD65\uD835\uDD60\uD835\uDD63\uD835\uDD64!");
+                model.addObject("greeting", "Good night, Investors!");
             } else if  (hrs >= 6 && hrs < 12) {
-                model.addObject("greeting", "\uD835\uDD3E\uD835\uDD60\uD835\uDD60\uD835\uDD55 \uD835\uDD5E\uD835\uDD60\uD835\uDD63\uD835\uDD5F\uD835\uDD5A\uD835\uDD5F\uD835\uDD58, \uD835\uDD40\uD835\uDD5F\uD835\uDD67\uD835\uDD56\uD835\uDD64\uD835\uDD65\uD835\uDD60\uD835\uDD63\uD835\uDD64!");
+                model.addObject("greeting", "Good morning, Investors!");
             } else if (hrs >= 12 && hrs <= 17) {
-                model.addObject("greeting", "\uD835\uDD3E\uD835\uDD60\uD835\uDD60\uD835\uDD55 \uD835\uDD52\uD835\uDD57\uD835\uDD65\uD835\uDD56\uD835\uDD63\uD835\uDD5F\uD835\uDD60\uD835\uDD60\uD835\uDD5F, \uD835\uDD40\uD835\uDD5F\uD835\uDD67\uD835\uDD56\uD835\uDD64\uD835\uDD65\uD835\uDD60\uD835\uDD63\uD835\uDD64!");
+                model.addObject("greeting", "Good afternoon, Investors!");
             } else {
-                model.addObject("greeting", "\uD835\uDD3E\uD835\uDD60\uD835\uDD60\uD835\uDD55 \uD835\uDD56\uD835\uDD67\uD835\uDD56\uD835\uDD5F\uD835\uDD5A\uD835\uDD5F\uD835\uDD58, \uD835\uDD40\uD835\uDD5F\uD835\uDD67\uD835\uDD56\uD835\uDD64\uD835\uDD65\uD835\uDD60\uD835\uDD63\uD835\uDD64!");
+                model.addObject("greeting", "Good evening, Investors!");
             }
         }
 
